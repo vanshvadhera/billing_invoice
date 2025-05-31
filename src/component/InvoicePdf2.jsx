f6a0c37ab44e2573529d494a78c9a410749407dc
vanshvadhera <vansh.vadhera009@gmail.com>
2025-05-31 23:51:39 +0530

    invoice pdf2

--- src/component/InvoicePdf2.jsx
+++ src/component/InvoicePdf2.jsx
@@ -7,9 +7,70 @@ export default function InvoicePdf2({ formData }) {
         <div className="bg-white p-3">
             <p className="text-center fs-4 text-black">TAX INVOICE</p>
 
-            <div className="row border mt-2 mx-3">
-                <div className="col-md-6 bg-danger" style={{ height: "150px" }}></div>
-                <div className="col-md-6 bg-success" style={{ height: "150px" }}></div>
+            <div className="table-responsive">
+                <table className="table table-bordered">
+                    <tbody>
+                        {/* Company Info + Invoice Info */}
+                        <tr>
+                            <td rowSpan="3" colSpan="2">
+                                <strong>AVID RISE DIGITAL SOLUTATIONS</strong><br />
+                                Azizganj Anand Vihaar Near S.S College<br />
+                                Shahjahanpur<br />
+                                GSTIN/UIN: 09ACEFA9697B1Z3<br />
+                                State Name: Uttar Pradesh, Code: 09<br />
+                                Contact : 9307489221
+                            </td>
+                            <td><strong>Invoice No.</strong></td>
+                            <td><strong>04</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Dated</strong></td>
+                            <td><strong>09-Apr-25</strong></td>
+                        </tr>
+                        <tr>
+                            <td><strong>Delivery Note</strong></td>
+                            <td>Mode/Terms of Payment</td>
+                        </tr>
+
+                        {/* Order, Dispatch & Terms */}
+                        <tr>
+                            <td colSpan="2">
+                                <strong>Consignee (Ship to)</strong><br />
+                                <strong>ULTRATECH CEMENT LIMITED</strong><br />
+                                SHAHJAHANPUR UNIT<br />
+                                GSTIN/UIN : 09AAACL6442L1Z8<br />
+                                State Name : Uttar Pradesh, Code : 09
+                            </td>
+                            <td><strong>Buyer's Order No.</strong></td>
+                            <td>Dated</td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan="2">
+                                <strong>Buyer (Bill to)</strong><br />
+                                <strong>ULTRATECH CEMENT LIMITED</strong><br />
+                                SHAHJAHANPUR UNIT<br />
+                                GSTIN/UIN : 09AAACL6442L1Z8<br />
+                                State Name : Uttar Pradesh, Code : 09<br />
+                                Place of Supply : Uttar Pradesh
+                            </td>
+                            <td><strong>Dispatch Doc No.</strong></td>
+                            <td>Delivery Note Date</td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan="2"></td>
+                            <td><strong>Dispatched through</strong></td>
+                            <td>Destination</td>
+                        </tr>
+
+                        <tr>
+                            <td colSpan="4">
+                                <strong>Terms of Delivery</strong>
+                            </td>
+                        </tr>
+                    </tbody>
+                </table>
             </div>
         </div>
     );
